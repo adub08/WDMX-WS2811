f5857be50ed37e18ff81d7e00bedf31fd327b110
Alex Woodmansey <awoodma1@our.ecu.edu.au>
2017-08-09 11:45:16 +0800

    Fixes made
    Fixes made + Improved notation

--- fastLED_DMX_3.ino
+++ fastLED_DMX_3.ino
@@ -1,20 +1,20 @@
-//Written by Alex Woodmansey using Version 1.6.8
+//Written by Alex Woodmansey using Version 1.6.8 - 19-06-2016
 #include "lib_dmx.h" //4 Universe DMX Library http://www.deskontrol.net/blog/arduino-four-universes-dmx-512-library/
 #include "FastLED.h" //Fast LED Library https://github.com/FastLED/FastLED
 #include <EEPROM.h>
 #define    DMX512     (0)    // (250 kbaud - 2 to 512 channels) Standard USITT DMX-512
-#define NUM_LEDS 64 //Number of LED Pixels connected
+#define NUM_LEDS 11 //Number of LED Pixels connected
 #define DATA_PIN 6  //Data Pin for LED Pixels
 CRGB leds[NUM_LEDS]; //Define LED Array
-#define COLOUR_ORDER GRB // LED colour order 
+#define COLOUR_ORDER RBG // LED colour order 
 
 const int ledPin =  5;      // LED Pin 5 for DMX reception yes/no
 const int testPin = 3;     // Switch Pin for Testing battery and panel
 const int disconnectPin = 2; // switch pin to disconnect wdmx
-const int WDMX_button = A5;     // WDMX Button Pin
+const int WDMX_button = A5;     // Internal WDMX Button Pin
 unsigned long now = 0; //current time value
 const byte channelwidth = 3; //3 DMX channels per pixel
-byte channel; //channel number for each pixel RGB
+int channel; //channel number for each pixel RGB
 volatile byte currentcounter = 0; //counter for DMX reception
 byte previouscounter = 0; //counter for DMX reception 
 byte current_hue; //hue value for rainbow effect
@@ -25,7 +25,7 @@ volatile int val; //analog voltage from
 
 void setup() 
 { 
-//  EEPROM.write(0, 1);
+//  EEPROM.write(0, 1);   //Alternative addressing method by holding the DMX address in the EEPROM 
 //  EEPROM.write(1, 64);
   ArduinoDmx0.attachRXInterrupt  (frame_received);  //set function called when all channels received
   ArduinoDmx0.set_control_pin (A2);    // control pin for max485   
@@ -38,13 +38,13 @@ void setup()
   pinMode (disconnectPin, INPUT_PULLUP); //Disconnect interrupt switch
   digitalWrite(WDMX_button, HIGH); //WDMX control pin left high
   LEDS.setBrightness(255); //Set overall LED brightness
-  FastLED.addLeds<WS2812, DATA_PIN, COLOUR_ORDER>(leds, NUM_LEDS); //initialize LED Pixel output
+  FastLED.addLeds<WS2811, DATA_PIN, COLOUR_ORDER>(leds, NUM_LEDS); //initialize LED Pixel output
   attachInterrupt(digitalPinToInterrupt(disconnectPin), disconect, LOW);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5,2700);  //#experiemental# sets max power at 13.5w for LEDS
+  FastLED.setMaxPowerInVoltsAndMilliamps(12,1250);  //#experiemental# sets max power at 13.5w for LEDS
   
 
   post(); //run power on self test function each time board resets 
-  SYS_TEST();
+  SYS_TEST(); //Checks Battery voltage and displays over pixels
 }  //end setup()
 
 void loop()
@@ -57,7 +57,7 @@ void loop()
   
   if((millis() - now) > 5000) //is the time since the value changed greater than 5 seconds?
   {
-   //digitalWrite(ledPin, LOW); //turn status LED off. We are no longer receiving the DMX signal.  
+   digitalWrite(ledPin, LOW); //turn status LED off. We are no longer receiving the DMX signal.  
     noDMX(); //run no DMX function
   }
   else
@@ -94,16 +94,29 @@ channel +=channelwidth; //increase last DMX channel number by channel width
 
 void noDMX() //Function to run when no DMX signal is present
 {
-    digitalWrite(ledPin, LOW); //turn status LED off. We are no longer receiving the DMX signal.
-    delay(500);
+    
+    
+  // fill 'leds' array with a rainbow, starting at
+  // a slightly different color each time, to create motion.
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1; /* higher = faster motion */
 
+  fill_rainbow( leds, NUM_LEDS, startIndex);
+
+  // tell FastLED to send the 'leds' buffer out to the
+  // actual LEDs
+  FastLED.show();
+  
+  // insert a delay to keep the framerate modest (eg, 60 FPS).
+  // delay takes milliseconds, to 1000/60 = about 60 FPS.
+  FastLED.delay(1000 / 60);
 
 } //End noDMX
 
-void SYS_TEST() //Will show battery voltage on a scale of 0-1023 over 0-NUM_LEDs [requires calabration]
+ void SYS_TEST() //Will show battery voltage on a scale of 0-1023 over 0-NUM_LEDs [requires calabration]
 {
   FastLED.clear(); //take all LEDs to 0
-  fill_solid( leds, NUM_LEDS, CRGB(255,255,255));
+  fill_solid( leds, NUM_LEDS, CRGB(255,255,255)); // All LEDs Full at white 
   FastLED.show();
   delay(300);
   int val = analogRead(0); //reads battery Voltage
@@ -115,6 +128,7 @@ void SYS_TEST() //Will show battery voltage on a scale of 0-1023 over 0-NUM_LEDs
   FastLED.show();
   delay(10000);
 }
+
 
 void post() //power on self test function.  Make sure all pixels are working and run through colors.
 {
@@ -128,7 +142,7 @@ void post() //power on self test function.  Make sure all pixels are working and
    
 }
 
-void disconect() //Disconnects WDMX module, runs within ISR - could be nicer. 
+ void disconect() //Disconnects WDMX module, currently runs within ISR - could be nicer. 
 {
   digitalWrite(WDMX_button,LOW);
   delayMicroseconds(30000);
@@ -137,3 +151,4 @@ void disconect() //Disconnects WDMX module, runs within ISR - could be nicer.
   delayMicroseconds(30000);
   digitalWrite(WDMX_button,HIGH);
 }
+
